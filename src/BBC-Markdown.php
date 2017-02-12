c432b14f7846e6257175a4a58f26528fabe1e851
C3realGuy <c3rgey@gmail.com>
2017-02-12 19:38:10 +0100

    removed possible xss, but still this is a poc and not ment for productional use

--- src/BBC-Markdown.php
+++ src/BBC-Markdown.php
@@ -2,8 +2,8 @@
 
 
 function bbc_process_markdown(&$tag, &$data, $disabled, $params) {
-  require_once dirname(__FILE__) . '/Parsedown.php';
-  require_once dirname(__FILE__) . '/ParsedownExtra.php';
+  require_once __DIR__ . '/../lib/Parsedown.php';
+  require_once __DIR__ . '/../lib/ParsedownExtra.php';
 
   log_error(var_export($tag, true).' '.$data);
 
@@ -11,8 +11,9 @@ function bbc_process_markdown(&$tag, &$data, $disabled, $params) {
   $ParsedownExtra = new ParsedownExtra();
 
   $data = str_replace('<br>', "\n", $data);
-  $data = html_entity_decode($data, ENT_QUOTES | ENT_XML1, 'UTF-8');
+
+  //$data = $Parsedown->text($data);
+
   file_put_contents('/tmp/test', $data);
-  $data = $Parsedown->text($data);
   $data = $ParsedownExtra->text($data);
 }
