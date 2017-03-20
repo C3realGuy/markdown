125d970f54316603a6c34b721d9cc60d0f0e5a9e
C3realGuy <c3rgey@gmail.com>
2017-03-20 19:22:29 +0100

    improved nearly everything, now use slightly modified css classes from github, actually you can now use it, and it looks ok

--- src/BBC-Markdown.php
+++ src/BBC-Markdown.php
@@ -1,19 +1,33 @@
 <?php
 
+/**
+ * Compatibility function for older wedge versions
+ */
+function bbc_validate_markdown(&$tag, &$data, &$disabled) {
+	bbc_process_markdown($tag, $data, $disabled, null);
+}
 
-function bbc_process_markdown(&$tag, &$data, $disabled, $params) {
+function bbc_process_markdown(&$tag, $content, $disabled, $params) {
   require_once __DIR__ . '/../lib/Parsedown.php';
   require_once __DIR__ . '/../lib/ParsedownExtra.php';
+  require_once __DIR__ . '/../lib/Michelf/Markdown.inc.php';
 
-  log_error(var_export($tag, true).' '.$data);
+  //log_error(var_export(['tag' => $tag, 'content' => $content, 'disabled' => $disabled, 'params' => $params], true));
+  add_plugin_css_file('CerealGuy:Markdown', 'css/github-markdown', true);
 
   $Parsedown = new Parsedown();
   $ParsedownExtra = new ParsedownExtra();
 
-  $data = str_replace('<br>', "\n", $data);
+  $content = str_replace('<br>', "\n", $content);
+  $content = str_replace('[url]', '', $content);
+  $content = str_replace('[/url]', '', $content);
+  $content = html_entity_decode($content);
 
-  //$data = $Parsedown->text($data);
+  file_put_contents('/tmp/test', $content);
 
-  file_put_contents('/tmp/test', $data);
-  $data = $ParsedownExtra->text($data);
+  $content = Parsedown::instance()
+   ->text($content);
+  $content = nl2br($content);
+  file_put_contents('/tmp/test2', $content);
+  $tag['content'] = '<div class="bbc_markdown markdown-body">' . $content . '</div>';
 }
